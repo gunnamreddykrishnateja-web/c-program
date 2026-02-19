406ca12202f33ccab7926ea3278132f8c562bfc3
g.krishnateja <gunnamreddykrishnateja@gmail.com>
2026-02-19 20:47:15 +0530

    Add files via upload

--- while/while 1.c
+++ while/while 1.c
@@ -4,14 +4,23 @@ int main() {
     scanf("%d",&n);
     int i=1;
     int count=0;
+     int streak=0,max=0;
     while(i<=n){
         int noise;
         scanf("%d",&noise);
         if(noise>70){
             count++;
+            streak++;
+        }
+        else{
+            streak=0;
+        }
+        if(streak>max){
+            max=streak;
         }
         i++;
     }
-    printf("%d",count);
+
+    printf("Noise Violation: %d\nLongest Violation streak: %d",count,max);
     return 0;
 }
